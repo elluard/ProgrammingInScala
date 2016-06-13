1ac32c8c9695adde1d29477fae4ae1fe40891f90
leehwangchun <elluard@me.com>
2016-06-13 22:06:02 +0900

    #예제 코드 커밋... 스칼라는 뭔가 신기한게 많다.

---
+++ Chapter9/src/FileMatherClosure.scala
@@ -0,0 +1,24 @@
+/**
+  * Created by leehwangchun on 2016. 6. 13..
+  */
+object FileMatherClosure {
+    private def filesHere = (new java.io.File(".")).listFiles;
+
+    private def filesMatching(matcher : String => Boolean) =
+        for(file <- filesHere; if matcher(file.getName))
+            yield file
+
+    def filesEnding(query : String) =
+        filesMatching(_.endsWith(query))
+
+    def filesContaining(query : String) =
+        filesMatching(_.contains(query))
+
+    def filesRegex(query:String) =
+        filesMatching(_.matches(query))
+
+    def main(args : Array[String]) : Unit = {
+        var output = filesEnding("iml");
+        println(output);
+    }
+}
